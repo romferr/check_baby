60744a42c03a7d1d6a8e4b66a97bde7c48dcbd72
romferr <19Ouik8!24>
2022-06-27 16:29:26 +0200

    add one-to-many relationship in Care table (foreign -> biberon) edit the html array fuck css

--- app.py
+++ app.py
@@ -1,30 +1,54 @@
 import sqlalchemy
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from sqlalchemy import Column, exc, Integer, String, Date, Boolean, MetaData, ForeignKey, Float
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:@localhost:3306/checkbaby_db")
-
 Base = declarative_base()
 
+engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:@localhost:3307/checkbaby_db")
+metadata = MetaData(engine)
 Session = sqlalchemy.orm.sessionmaker()
 Session.configure(bind=engine)
 session = Session()
 
 
+class Biberon(Base):
+    __tablename__ = 'biberons'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(16), default='Lait')
+    ml = Column(Float, default=0)
+
+    def __repr__(self):
+        return f'<Post "{self.id}">'
+
+
 class Care(Base):
-    __tablename__ = 'care'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    date = sqlalchemy.Column(sqlalchemy.DATE)
-    pipi = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
-    selle = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
-    vitamine_D = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
-    yeux = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
-    nez = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
-    peau = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
-    bain = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+    __tablename__ = 'cares'
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    biberon_id = Column(Integer, ForeignKey('biberons.id'))
+    pipi = Column(Boolean, default=False)
+    selle = Column(Boolean, default=False)
+    vitamine_D = Column(Boolean, default=False)
+    yeux = Column(Boolean, default=False)
+    nez = Column(Boolean, default=False)
+    peau = Column(Boolean, default=False)
+    bain = Column(Boolean, default=False)
+    biberon = relationship("Biberon", back_populates="cares")
+
+    def __repr__(self):
+        return f'<Post "{self.id}">'
+
+
+Biberon.cares = relationship("Care", order_by=Care.id, back_populates="biberon")
+try:
+    Base.metadata.create_all(engine)
+except exc.SQLAlchemyError as error:
+    print(f"{error.code} - {error.args}")
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -42,4 +66,4 @@ def index():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run(Debug=True)
+    app.run(debug=True)
